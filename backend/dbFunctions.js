070e9234cf3ebd48349344dc74202dfd75476fac
XcGXC <98114686+XcGXC@users.noreply.github.com>
2022-12-04 10:26:02 -0500

    git commit

--- backend/dbFunctions.js
+++ backend/dbFunctions.js
@@ -218,6 +218,107 @@ const getSpeciesAbundanceByState = async (state) => {
   }
 };
 
+//Query7: filter species according to multiple conditions:
+const filterSpecies = async (park,family,order) => {
+  try {
+    const db = await getDB();
+    const query = " WITH Temp AS (\
+					SELECT ID, O.Scientific_Name, Family, Park_Name, State\
+					FROM Occurrence O JOIN National_Park NP ON O.Park_ID = NP.Code\
+						JOIN Species S ON S.Scientific_Name = O.Scientific_Name\
+					)\
+					SELECT t1.Family, t1.Park_Name, COUNT(*) AS NUM\
+					FROM Temp t1, Temp t2\
+					WHERE t1.Park_Name = t2.Park_Name AND t1.Park_Name = ?\
+					AND t1.Family = ?\
+					AND t1.Family = t2.Family AND t1.Scientific_Name <> t2.Scientific_Name\
+					GROUP BY t1.Family\
+					ORDER BY NUM ?, t1.Family";
+    const params = [park,family,order];
+    const [rows] = await db.execute(query, params);
+    return rows;
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    throw new Error('Error executing the query');
+  }
+};
+
+//Query8: filter park by fire suffer:
+const getParksByFireSuffer = async (percent) => {
+  try {
+    const db = await getDB();
+    const query = " WITH TEMP AS (SELECT Scientific_Name, COUNT(*)\
+					FROM Wild_fire W\
+						JOIN Occurrence O on W.National_park=O.Park_Name\
+					GROUP BY Scientific_Name\
+					ORDER BY COUNT(*) DESC\
+					LIMIT ?*4.83)\
+					SELECT Name\
+					FROM National_Park N JOIN Occurrence O on N.Name=O.Park_Name\
+					WHERE O.Scientific_Name IN (SELECT Scientific_Name  FROM TEMP)\
+					GROUP BY N.Name\
+					HAVING COUNT(*)>(SELECT 0.01*COUNT(*)\
+                         FROM National_Park N2\
+                         WHERE N2.Name=N.Name\   GROUP BY N2.Name)\
+					ORDER BY  N.Name";
+	const params = [percent];
+    const [rows] = await db.execute(query, params);
+    return rows;
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    throw new Error('Error executing the query');
+  }
+};
+
+//Query9: Find all Species Order distribution in different parks in a State 
+const OrderListInState = async (state) => {
+  try {
+    const db = await getDB();
+    const query = " SELECT FO.SpeciesOrder, O.Park_Name, COUNT(*) AS NUM\
+					FROM Occurrence O JOIN Species S ON O.Scientific_Name = S.Scientific_Name\
+						JOIN Family_Order FO ON FO.Family = S.Family\
+					WHERE O.Park_Name IN (\
+					SELECT NP.Name FROM National_Park NP WHERE NP.state = ?)\
+					GROUP BY FO.SpeciesOrder, O.Park_Name\
+					ORDER BY NUM DESC";
+	const params = [state];
+    const [rows] = await db.execute(query, params);
+    return rows;
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    throw new Error('Error executing the query');
+  }
+};
+
+//Query10: Find all Species in certain category live in some state but not some other state without severe fire
+const FindSpeciesInNotIn = async (category,in_state1,in_state2,not_in_state3) => {
+  try {
+    const db = await getDB();
+    const query = " WITH temp as (\
+					SELECT s.Scientific_Name, O.Park_Name\
+					FROM Species s left join  Family_Order FO on s.Family = FO.Family\
+					left join Order_Category OC on FO.SpeciesOrder = OC.SpeciesOrder\
+					join Occurrence O on s.Scientific_Name = O.Scientific_Name\
+					WHERE OC.Category = ?\
+					)\
+					SELECT temp.Scientific_Name, temp.Park_Name\
+					FROM temp join National_Park on temp.Park_Name = National_Park.Name\
+						join Wild_fire Wf on National_Park.State = Wf.STATE\
+					WHERE Park_Name in (SELECT Name FROM National_Park np WHERE np.State = ? or np.State = ? )\
+					AND Wf.FIRE_SIZE != 'A'\
+					AND temp.Scientific_Name not in (\
+					SELECT Scientific_Name\
+					FROM temp\
+					WHERE temp.Park_Name in (SELECT Name FROM National_Park np WHERE np.State = '?));";
+	const params = [category,in_state1,in_state2,not_in_state3];
+    const [rows] = await db.execute(query, params);
+    return rows;
+  } catch (err) {
+    console.log(`Error: ${err.message}`);
+    throw new Error('Error executing the query');
+  }
+};
+
 
 
 module.exports = {
