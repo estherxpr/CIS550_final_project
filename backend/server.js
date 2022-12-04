34b5e14405d748b72e8b2df3e93c37235cb1ffe6
MinzhengZhang <minzheng@seas.upenn.edu>
2022-12-04 03:53:02 -0500

    updated express server, create index.js file

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,20 @@
 const express = require('express');
 
-
 // Create express app
-const webapp = express();
+const app = express();
+const cors = require('cors');
 
-// Import database operations
+app.use(cors({ credentials: true, origin: ['http://localhost:3000'] }));
+app.use(express.urlencoded({ extended: true }), express.json());
 const lib = require('./dbFunctions');
 
 // Root endpoint
-webapp.get('/', (req, res) => {
-  res.json({ message: 'Welcome to our web app' });
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to CIS550 Project' });
 });
 
 // Other API endpoints
-webapp.get('/parks', async (req, res) => {
+app.get('/parks', async (req, res) => {
   try {
     const results = await lib.getAllParks();
     res.status(200).json({ data: results });
@@ -22,36 +23,158 @@ webapp.get('/parks', async (req, res) => {
   }
 });
 
-webapp.get('/parks/name/:name', async (req, res) => {
-    try {
-        const results = await lib.getParkByName(req.params.name);
-        res.status(200).json({ data: results });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
+app.get('/parks/:park', async (req, res) => {
+  const { park } = req.params;
+  try {
+    let result;
+    if (park.length === 4) {
+      result = await lib.getParkByCode(park);
+    } else {
+      result = await lib.getParkByName(req.params.name);
     }
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 });
 
-webapp.get('/parks/code/:code', async (req, res) => {
-    try {
-        const results = await lib.getParkByCode(req.params.code);
-        res.status(200).json({ data: results });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
-    }
+app.get('/species/:species', async (req, res) => {
+  try {
+    const { species } = req.params;
+    // let result;
+    // check if argument species is the species Id or the species name
+    // if (species.length === 9 && !Number.isNaN(parseInt(species.substring(5, 9), 10))) {
+    //   result = await lib.getSpeciesByCode(species);
+    //   // seems we do not have species Id in the database
+    // } else {
+    const result = await lib.getSpeciesByName(species);
+    // }
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 });
 
-webapp.get('/species/name/:name', async (req, res) => {
-    try {
-        const results = await lib.getSpeciesByName(req.params.name);
-        res.status(200).json({ data: results });
-    } catch (err) {
-        res.status(404).json({ error: err.message });
+app.get('/trades/:species', async (req, res) => {
+  try {
+    const { species } = req.params;
+    const result = await lib.getTradesBySpecies(species);
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+// get a colleciton of states, for each state, it contains basic information of state
+app.get('/states', async (req, res) => {
+  try {
+    const results = await lib.getAllStates();
+    res.status(200).json({ data: results });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+// get a specific state general info
+app.get('/states/:state', async (req, res) => {
+  const { state } = req.params;
+  try {
+    const result = await lib.getStateByName(state);
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+app.get('/states/:state/parks', async (req, res) => {
+  const { state } = req.params;
+  try {
+    const result = await lib.getParksByState(state);
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+app.get('/states/:state/species', async (req, res) => {
+  const { state } = req.params;
+  try {
+    const result = await lib.getSpeciesByState(state);
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+app.get('/states/:state/fires', async (req, res) => {
+  const { state } = req.params;
+  try {
+    const result = await lib.getFiresByState(state);
+    res.status(200).json({ data: result });
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
+});
+
+// main idea to deal with complex query
+// we can divide query into several parts,
+// one part is general/common part{orderBy,limit, groupBy},
+// another part is specific part{species, state, park, percent, etc}
+// we probably can use higher order function to handle this
+// it is like a tree structure, each node indicate a filter or a operation
+// search/parks indicates that we are searching results for parks
+app.get('/search/parks?', async (req, res) => {
+  // here using keyword to switch, and use expectedFiled to select the field we want to search
+  // every single query can be a list, connected by #
+  const {
+    keyword, expectedFiled,
+    isexpectedFiledUnique,
+    groupBy,
+    orderBy,
+    limit,
+    percent,
+    fireClass,
+  } = req.query;
+  if (!keyword || !expectedFiled) {
+    res.status(400).json({ error: 'Please provide a keyword or park name' });
+  }
+  try {
+    switch (keyword) {
+      case 'fireSuffer': { // this correlated to query 8
+        const args = {
+          percent,
+          orderBy,
+          expectedFiled,
+          isexpectedFiledUnique,
+          limit,
+          groupBy,
+        };
+        const result = await lib.getParksByFireSuffer(args);
+        res.status(200).json({ data: result });
+        return;
+      }
+      case ('fireClass'): { // this correlated to query 5
+        const args = {
+          fireClass, orderBy, expectedFiled, limit, isexpectedFiledUnique, groupBy,
+        };
+        const result = await lib.getParksByFireClass(args);
+        res.status(200).json({ data: result });
+        return;
+      }
+
+      default: {
+        res.status(404).json({ message: 'No such keyword' });
+        return;
+      }
     }
+  } catch (err) {
+    res.status(404).json({ error: err.message });
+  }
 });
 
 // Default response for any other request
-webapp.use((_req, res) => {
+app.all((req, res) => {
   res.status(404);
 });
 
-module.exports = webapp; // for testing
+module.exports = app; // for testing
